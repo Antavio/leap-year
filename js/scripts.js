dbd11cfc804d96e43a3e0a07aa6d4436cd7f6778
Anthony Njuguna <njuguna13@gmail.com>
2019-02-14 13:03:12 +0300

    Added a function to the scripts file.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function leapyear() {
+  var input = parseInt(document.getElementById("year").value);
+  if(input % 4 ===0 && input % 100 !== 0){
+
+    alert(input + " is a leap year");
+} else if (input % 100 ===0 && input % 400 === 0) {
+
+    alert(input + " is a leap year");
+} else {
+    alert(input + " is not a leap year");
+}
+}
