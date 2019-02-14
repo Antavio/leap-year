04b77146a5b96a10f9c8a8b526ae481a5a4dbc92
Anthony Njuguna <njuguna13@gmail.com>
2019-02-14 15:46:22 +0300

    Modified the output alert commands.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ function leapyear() {
   var input = parseInt(document.getElementById("year").value);
   if(input % 4 ===0 && input % 100 !== 0){
 
-    alert(input + " is a leap year");
+    alert("True");
   } else if (input % 100 ===0 && input % 400 === 0) {
 
-    alert(input + " is a leap year");
+    alert(" True");
   } else {
-    alert(input + " is not a leap year");
+    alert("False");
   }
 }
